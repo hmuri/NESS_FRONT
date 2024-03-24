346eaff3b0c9be5c107b62ef2c63687818511431
hmuri <113418916+hmuri@users.noreply.github.com>
2024-03-24 15:43:54 +0900

    [feat] 기본 화면 구성 완성

--- ness/src/pages/_app.tsx
+++ ness/src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--vw", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
   return <Component {...pageProps} />;
 }
