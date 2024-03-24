5dd587fee37420e92490892f9fc49e0e84bd3dc2
hmuri <113418916+hmuri@users.noreply.github.com>
2024-03-24 16:58:00 +0900

    [feat] 채팅 완성

--- ness/src/pages/_app.tsx
+++ ness/src/pages/_app.tsx
@@ -7,10 +7,14 @@ export default function App({ Component, pageProps }: AppProps) {
     let vh = window.innerHeight * 0.01;
     let vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-    document.documentElement.style.setProperty("--vw", `${vh}px`);
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
   }
   useEffect(() => {
     setScreenSize();
-  });
+    window.addEventListener("resize", setScreenSize);
+    return () => {
+      window.removeEventListener("resize", setScreenSize);
+    };
+  }, []);
   return <Component {...pageProps} />;
 }
--- ness/src/pages/chatting/index.tsx
+++ ness/src/pages/chatting/index.tsx
@@ -20,34 +20,43 @@ const Chatting = () => {
   };
 
   return (
-    <div className="min-h-100vh w-full bg-[#F2F0FF] p-[20px]">
-      <div>
-        {chatMessages.map((message, index) => {
-          const isUser = message.user === "user";
-          return (
-            <div
-              key={index}
-              className={`flex ${isUser ? "justify-end" : "justify-start"}`}
-            >
+    <div className="flex flex-col h-screen">
+      <div className="border-b border-gray-200 bg-white w-full h-[94px] fixed mt-[60px] relative flex justify-center items-center">
+        네스
+      </div>
+      <div className="relative flex-1 w-full bg-[#F2F0FF] py-[26px]">
+        <div className="px-[20px]">
+          {chatMessages.map((message, index) => {
+            const isUser = message.user === "user";
+            return (
               <div
-                className={`max-w-xs px-4 py-2 rounded-lg ${
-                  isUser
-                    ? "bg-purple-500 text-white rounded-br-none"
-                    : "bg-white text-gray-800 rounded-bl-none"
+                key={index}
+                className={`flex mb-[14px] ${
+                  isUser ? "justify-end" : "justify-start"
                 }`}
               >
-                <p>{message.content}</p>
+                <div
+                  className={`max-w-[70%] px-[12px] py-[10px] rounded-[16px] ${
+                    isUser ? "bg-[#7A64FF] text-white" : "bg-white text-black"
+                  }`}
+                >
+                  <p>{message.content}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="absolute bottom-[25px] left-[20px] right-[20px] flex">
+          <input
+            className="w-full bg-white h-[41px] px-[22px] py-[13px] rounded-[20px] border border-purple-600 shadow-md"
+            type="text"
+            value={newMessage}
+            placeholder="채팅 입력하기"
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button onClick={handleSendMessage}>send</button>
+        </div>
       </div>
-      <input
-        type="text"
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-      />
-      <button onClick={handleSendMessage}>send</button>
     </div>
   );
 };
