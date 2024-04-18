2e2392557d5747072c99e188ffe593fb2b9cc92b
hmuri <minju.choi@ringleplus.com>
2024-04-18 19:56:48 +0900

    chatting image add

--- ness/src/pages/chatting/index.tsx
+++ ness/src/pages/chatting/index.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import NessImg from "../../assets/ness_chat.png";
 import { IChatMessage, ISendMessage } from "../../module/interface/chatting";
 import Cookies from "universal-cookie";
+import SendImg from "../../assets/send.png";
+import LeftChatImg from "../../assets/leftChat.png";
+import RightChatImg from "../../assets/rightChat.png";
 
 const cookies = new Cookies();
 
@@ -11,6 +14,13 @@ const Chatting = () => {
   const [chatMessages, setChatMessages] = useState<IChatMessage[]>([]);
   const [newMessage, setNewMessage] = useState("");
   const [accessToken, setAccessToken] = useState("");
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chatMessages]);
 
   useEffect(() => {
     const token = cookies.get("accessToken") || "";
@@ -64,7 +74,7 @@ const Chatting = () => {
           {chatMessages.map((message, index) => (
             <div
               key={index}
-              className={`flex mb-[14px] ${
+              className={`flex relative mb-[14px] ${
                 message.chatType === "USER" ? "justify-end" : "justify-start"
               }`}
             >
@@ -75,20 +85,32 @@ const Chatting = () => {
                     : "bg-white text-black"
                 }`}
               >
+                <Image
+                  src={message.chatType === "USER" ? RightChatImg : LeftChatImg}
+                  className={`absolute bottom-3 ${
+                    message.chatType === "USER"
+                      ? "right-[-11px]"
+                      : "left-[-11px]"
+                  }`}
+                  alt=""
+                />
                 <p>{message.text}</p>
               </div>
             </div>
           ))}
+          <div ref={messagesEndRef} />
         </div>
-        <div className="fixed bg-[#F2F0FF] h-[66px] bottom-0 left-[20px] right-[20px] flex">
+        <div className="fixed bg-[#F2F0FF] h-[66px] bottom-0 left-[20px] right-[20px] flex items-center">
           <input
-            className="w-full bg-white h-[41px] px-[22px] py-[13px] rounded-[20px] border border-purple-600 shadow-md"
+            className="w-full bg-white h-[41px] px-[22px] py-[13px] mr-[8px] rounded-[20px] border border-purple-600 shadow-md"
             type="text"
             value={newMessage}
             placeholder="채팅 입력하기"
             onChange={(e) => setNewMessage(e.target.value)}
           />
-          <button onClick={handleSendMessage}>send</button>
+          <button onClick={handleSendMessage} disabled={!newMessage.trim()}>
+            <Image src={SendImg} alt="" width={30} height={30} />
+          </button>
         </div>
       </div>
       <div className="z-100 border-b border-gray-200 bg-white w-full h-[94px] gap-[5px] fixed top-0 flex-col flex justify-center items-center">
