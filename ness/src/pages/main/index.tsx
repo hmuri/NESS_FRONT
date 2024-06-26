86eb3cf82fefef7c1ed7474044b6910f528b1f66
hmuri <113418916+hmuri@users.noreply.github.com>
2024-06-26 17:21:16 +0900

    입력창 추가

--- ness/src/pages/main/index.tsx
+++ ness/src/pages/main/index.tsx
@@ -13,15 +13,19 @@ import {
   Icon_big_normal_ness,
   Icon_calmness,
   Icon_hardness,
+  Icon_mic,
   Icon_normal,
 } from "@/module/icons";
 import Slider from "react-slick";
+import useSpeechRecognition from "@/module/hooks/speechRecognition";
+import StopIcon from "../../../public/assets/Stop button.png";
 
 const cookies = new Cookies();
 const token = cookies.get("accessToken") || "";
 
 const Main = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [newMessage, setNewMessage] = useState("");
   const settings = {
     dots: true,
     infinite: true,
@@ -55,8 +59,17 @@ const Main = () => {
   const [scheduleList, setScheduleList] = useState<
     ScheduleItem[] | undefined
   >();
+  const [isSTT, setIsSTT] = useState(false);
+  const { isListening, stopListening, startListening } =
+    useSpeechRecognition(setNewMessage);
 
   const imageUrls = [
+    "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-1.png",
+    "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-2.png",
+    "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-3.png",
+  ];
+
+  const images = [
     "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-1.png",
     "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-2.png",
     "https://ness-static-s3.s3.ap-northeast-2.amazonaws.com/background-3.png",
@@ -84,6 +97,14 @@ const Main = () => {
     fetchProfile();
   }, []);
 
+  const toggleListening = () => {
+    if (isListening) {
+      stopListening();
+    } else {
+      startListening();
+      setIsSTT(true);
+    }
+  };
   const handleImageError = (
     event: React.SyntheticEvent<HTMLImageElement>,
     index: number
@@ -190,7 +211,7 @@ const Main = () => {
           </div>
         </div>
 
-        {/* <div className="day-modal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="day-modal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="bg-white w-[348px] h-[700px] px-[25px] rounded-[20px] pt-[9px] pb-[20px] overflow-auto">
             <Slider {...settings}>
               {images.map((img, index) => (
@@ -200,8 +221,48 @@ const Main = () => {
               ))}
             </Slider>
           </div>
-        </div> */}
-
+        </div>
+        <div className="rounded-[15px] px-[30px] py-[10px] bg-[#F2F0FF] w-full md:max-w-[600px] flex flex-col items-center">
+          <div className="mb-[15px]">
+            추가하고 싶은 일정을 네스에게 말해주세요!
+          </div>
+          <input
+            className="w-full bg-white h-[41px] px-[22px] py-[13px] mr-[8px] mb-[10px] rounded-[20px] border border-purple-600 shadow-md"
+            type="text"
+            value={newMessage}
+            placeholder={isListening ? "듣는 중" : "채팅 입력하기"}
+            onChange={(e) => setNewMessage(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                // shiftKey를 누르지 않은 상태에서 Enter를 눌렀을 경우
+                e.preventDefault(); // Form 전송을 방지합니다.
+                // handleSendMessage(); // 메시지 전송 함수 호출
+              }
+            }}
+          />
+          {/* <div className="right-[70px] cursor-pointer">
+            {isListening ? (
+              <Image src={StopIcon} alt="" onClick={toggleListening} />
+            ) : (
+              <Icon_mic onClick={toggleListening} />
+            )}
+          </div> */}
+          {/* <button onClick={handleSendMessage} disabled={isLoading}></button> */}
+          {/* <button onClick={} disabled={!newMessage.trim()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+            >
+              <path
+                d="M11.0001 0C11.2043 0.000109065 11.4045 0.0570724 11.5782 0.16451C11.7519 0.271948 11.8922 0.425618 11.9835 0.60831L21.8843 20.4086C21.977 20.5945 22.0152 20.8027 21.9945 21.0094C21.9738 21.216 21.895 21.4125 21.7673 21.5763C21.6396 21.74 21.4681 21.8643 21.2727 21.9346C21.0773 22.005 20.866 22.0186 20.6632 21.974L11.0001 19.8267L1.33804 21.974C1.13514 22.0189 0.923671 22.0055 0.728101 21.9352C0.532531 21.8649 0.360865 21.7407 0.232979 21.5769C0.105093 21.4131 0.0262184 21.2165 0.00548795 21.0097C-0.0152425 20.803 0.0230191 20.5945 0.115843 20.4086L10.0166 0.60831C10.1079 0.425618 10.2482 0.271948 10.4219 0.16451C10.5956 0.0570724 10.7958 0.000109065 11.0001 0ZM12.1002 17.8181L18.8833 19.3251L12.1002 5.75969V17.8181Z"
+                fill={newMessage.trim() ? "#7A64FF" : "#777777"}
+              />
+            </svg>
+          </button> */}
+        </div>
         <div className="text-[20px] w-full font-[500] mb-[10px] mt-[40px] text-left  md:max-w-[600px]">
           앞으로의 일정을 확인해봐요!
         </div>
