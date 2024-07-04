1875a01c4b3b64aa3bc866336483c2841b26ca5c
hmuri <113418916+hmuri@users.noreply.github.com>
2024-07-04 13:25:24 +0900

    pwa 빌드 에러 수정

--- ness/next.config.mjs
+++ ness/next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 import withSvgr from "@newhighsco/next-plugin-svgr";
+import withPWA from "next-pwa";
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
-const withPWA = require("next-pwa")({
+const pwaConfig = withPWA({
   dest: "public", // 서비스 워커 파일이 저장될 위치
   disable: process.env.NODE_ENV === "development", // 개발 환경에서는 비활성화
 });
-
-export default withPWA(withSvgr(nextConfig));
+export default withSvgr(pwaConfig(nextConfig));
