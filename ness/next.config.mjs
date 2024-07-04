3594ba8d443f65731dbba6d14dfd7445b100a4a5
hmuri <113418916+hmuri@users.noreply.github.com>
2024-07-04 13:20:52 +0900

    홈화면 바로가기 추가

--- ness/next.config.mjs
+++ ness/next.config.mjs
@@ -5,4 +5,9 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withSvgr(nextConfig);
+const withPWA = require("next-pwa")({
+  dest: "public", // 서비스 워커 파일이 저장될 위치
+  disable: process.env.NODE_ENV === "development", // 개발 환경에서는 비활성화
+});
+
+export default withPWA(withSvgr(nextConfig));
