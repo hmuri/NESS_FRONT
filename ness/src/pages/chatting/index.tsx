fed6ede52599d92fe812afe00e880a3502b585b7
hmuri <minju1326@ewhain.net>
2024-07-18 01:31:14 +0900

    채팅 말꼬리 사라진 거 수정

--- ness/src/pages/chatting/index.tsx
+++ ness/src/pages/chatting/index.tsx
@@ -417,6 +417,21 @@ const Chatting = () => {
                             : "bg-white text-black"
                         } px-[12px] py-[10px] rounded-[16px]`}
                       >
+                        <Image
+                          src={
+                            message.chatType === "USER" ||
+                            message.chatType === "STT"
+                              ? RightChatImg
+                              : LeftChatImg
+                          }
+                          className={`absolute bottom-3 ${
+                            message.chatType === "USER" ||
+                            message.chatType === "STT"
+                              ? "right-[-11px]"
+                              : "left-[-11px]"
+                          }`}
+                          alt=""
+                        />
                         <p>{message.text}</p>
                       </div>
                     </div>
@@ -519,6 +534,21 @@ const Chatting = () => {
                           : "bg-white text-black"
                       } px-[12px] py-[10px] rounded-[16px]`}
                     >
+                      <Image
+                        src={
+                          message.chatType === "USER" ||
+                          message.chatType === "STT"
+                            ? RightChatImg
+                            : LeftChatImg
+                        }
+                        className={`absolute bottom-3 ${
+                          message.chatType === "USER" ||
+                          message.chatType === "STT"
+                            ? "right-[-11px]"
+                            : "left-[-11px]"
+                        }`}
+                        alt=""
+                      />
                       <p>{message.text}</p>
                     </div>
                   </div>
@@ -602,6 +632,21 @@ const Chatting = () => {
                             : "bg-white text-black"
                         } px-[12px] py-[10px] rounded-[16px]`}
                       >
+                        <Image
+                          src={
+                            message.chatType === "USER" ||
+                            message.chatType === "STT"
+                              ? RightChatImg
+                              : LeftChatImg
+                          }
+                          className={`absolute bottom-3 ${
+                            message.chatType === "USER" ||
+                            message.chatType === "STT"
+                              ? "right-[-11px]"
+                              : "left-[-11px]"
+                          }`}
+                          alt=""
+                        />
                         <p>{message.text}</p>
                       </div>
                     </div>
@@ -695,6 +740,21 @@ const Chatting = () => {
                         : "bg-white text-black"
                     } max-w-[70%] px-[12px] py-[10px] rounded-[16px]`}
                   >
+                    <Image
+                      src={
+                        message.chatType === "USER" ||
+                        message.chatType === "STT"
+                          ? RightChatImg
+                          : LeftChatImg
+                      }
+                      className={`absolute bottom-3 ${
+                        message.chatType === "USER" ||
+                        message.chatType === "STT"
+                          ? "right-[-11px]"
+                          : "left-[-11px]"
+                      }`}
+                      alt=""
+                    />
                     <p>{message.text}</p>
                   </div>
                 </div>
